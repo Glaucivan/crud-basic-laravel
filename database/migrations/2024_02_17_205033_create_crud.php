6343cffc9535666dfd92913da88c07633d4628f0
Glaucivan <glaucivanp2@gmail.com>
2024-02-17 18:09:21 -0300

    atualizado campo

--- database/migrations/2024_02_17_205033_create_crud.php
+++ database/migrations/2024_02_17_205033_create_crud.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('crud', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('registro');
             $table->timestamps();
         });
     }
